d1d3b24485750d67e0a7f7e9dad244e3e02111e8
alifndaru <alifndaru8@gmail.com>
2024-09-15 11:39:01 +0700

    added text thank you if success submit survei

--- resources/views/users/components/auth-header.blade.php
+++ resources/views/users/components/auth-header.blade.php
@@ -1,4 +1,9 @@
  <div class="login-brand">
      <img src="{{ asset('img/stisla-fill.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
      <h1 class="mt-4">Survei Apps</h1>
+     <p class="text-center" style="font-size: 10px">
+         Selamat Datang pada Aplikasi Survey terhadap Anak, Untuk Mengetahui Parenting Masyarakat
+         Terhadap Tumbuh Kembang Anak. Data yang kalian berikan sangat penting agar kita bersama menyiapkan Calon
+         Generasi Hebat dan handal.
+     </p>
  </div>
