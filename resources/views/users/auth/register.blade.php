80aad2890996be85991409d304e1caf5c5fa9562
alifndaru <alifndaru8@gmail.com>
2024-11-08 09:58:36 +0700

    feat: Update registration form to use number input for phone number field

--- resources/views/users/auth/register.blade.php
+++ resources/views/users/auth/register.blade.php
@@ -20,7 +20,7 @@
                 <div class="row">
                     <div class="form-group col-6">
                         <label for="no_HP">No HP</label>
-                        <input id="no_hp" type="text" class="form-control" name="no_hp" autofocus>
+                        <input id="no_hp" type="number" class="form-control" name="no_hp" autofocus>
                         <label for="age">Umur</label>
                         <input id="age" type="number" class="form-control" name="age" autofocus>
                     </div>
