3b64353f7546852c39b8060e5c893b200f513238
alifndaru <alifndaru8@gmail.com>
2024-11-10 15:53:23 +0700

    chore: Update footer design in dashboard

--- resources/views/dashboard/components/footer.blade.php
+++ resources/views/dashboard/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2024 <div class="bullet"></div> Design By Alif ndaru kusuma</a>
+        Copyright &copy; 2024 <div class="bullet"></div> Design</a>
     </div>
     <div class="footer-right">
         2.3.0
